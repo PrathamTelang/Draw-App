faf37058eb308b1594403e5df5af86472e39092d
PrathamTelang <prathamtelang007@gmail.com>
2025-07-31 22:19:23 +0530

    Add signup and signin endpoints to backend
    Introduces POST /api/v1/signup and /api/v1/signin routes for user registration and authentication. Uses JWT for token generation and moves port configuration to a config file.

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,5 +1,41 @@
 import express from "express";
+import jwt from "jsonwebtoken"
+import { config } from './config';
 
 const app = express()
 
-app.listen(3001)
+app.listen(config.port)
+
+app.post("api/v1/signup", async (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    await UserModel.create({
+        username,
+        password
+    })
+    res.json({
+        message: "user signed in"
+    })
+})
+
+app.post("api/v1/signin", async (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password
+
+    const existingUser = await UserModel.findOne({
+        username,
+        password
+        
+    })
+    if (existingUser) {
+        const token = jwt.sign(
+            {id: existingUser._id},
+            config.jwtSecret
+        );
+    }
+    
+    res.json({
+        message: "User signed in"
+    })
+})
