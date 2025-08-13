5eaaf4c1b8766cd6c986e08155a08ab8854ef885
PrathamTelang <prathamtelang007@gmail.com>
2025-08-13 13:26:18 +0530

    Add Tailwind CSS to web app and update UI
    Integrated Tailwind CSS into the web app by adding Tailwind and PostCSS dependencies, configuration files, and updating the global CSS to use Tailwind. Refactored the home page UI to use Tailwind utility classes for layout and styling.

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -9,13 +9,19 @@ export default function Home() {
   const router = useRouter()
 
   return (
-    <div className={styles.page}>
-      <input value={roomId} onChange={(e) => {
+    <div className={"bg-neutral-700 w-screen h-screen flex justify-center iteams-center"}>
+      <div className="flex justify-center items-center">
+        <input className="p-4 border-2 rounded-md" value={roomId} onChange={(e) => {
         setRoomId(e.target.value)
       }} type="text" placeholder="Roomid"></input>
-      <button onClick={() => {
-        router.push(`/room/${roomId}`)
-      }}>Join Room</button>
+      <button 
+        className="p-4 border-2 rounded-md" 
+        onClick={() => {
+          router.push(`/room/${roomId}`)
+        }}>
+          Join Room
+      </button>
+      </div>
     </div>
   );
 }
---
+++ apps/web/tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx}", // adjust based on your folder structure
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
