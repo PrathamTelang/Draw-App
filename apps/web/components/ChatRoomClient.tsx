5fa8008c99de2fb3a16ef749c50983da789365f2
PrathamTelang <prathamtelang007@gmail.com>
2025-08-15 13:40:41 +0530

    Add real-time chat updates with socket integration
    Introduced useEffect and useSocket to enable real-time message updates in ChatRoomClient. Incoming chat messages are now appended to the chat state when received via the socket.

--- apps/web/components/ChatRoomClient.tsx
+++ apps/web/components/ChatRoomClient.tsx
@@ -1,9 +1,26 @@
 "use client";
 
+import { useEffect, useState } from "react";
+import { useSocket } from "../hooks/useSocket";
+
 export function ChatRoomClient({
     messages,
     id
 }: {
     messages: {messages: string}[];
     id: string
-})
+}) {
+    const [chats, setChats] = useState(messages);
+    const {socket, loading} = useSocket();
+
+    useEffect(() => {
+        if (socket && loading) {
+            socket.onmessage = (event) => {
+                const parsedData = JSON.parse(event.data);
+                if (parsedData.type === "chat") {
+                    setChats(c => [...c, parsedData.message])
+                }
+            }
+        }
+    })
+}
